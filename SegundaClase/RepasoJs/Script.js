11ce6e73555fd2e72ade16d8015e836057699493
Ana Maria Blanco Villamizar <PruebaCorre@gmail.com>
2024-08-20 21:05:27 -0500

    Añadiendo Taller 1

---
+++ SegundaClase/RepasoJs/Script.js
@@ -0,0 +1,16 @@
+// Ejercicio 1 : Devolver por medio de una funcion un numro par o impar
+
+ 
+function esImapar(num){
+
+    if(num%2==0){
+
+        return "El numero es par";
+    }else{
+
+        return "El nuemero es impar";
+    }
+}
+
+ console.log(esImapar(5));
+
---
+++ taller1/script.js
@@ -0,0 +1,35 @@
+ 
+var button = document.getElementById('addTaskBtn');
+var input = document.getElementById('taskInput');
+var list = document.getElementById('taskList');
+
+ 
+button.addEventListener('click', function() {
+     
+    var text = input.value;
+
+     
+    if (text) {
+         
+        var taskButton = document.createElement('button');
+         
+
+         
+        var deleteButton = document.createElement('button');
+         
+
+         
+        deleteButton.addEventListener('click', function() {
+             
+            list.removeChild(taskButton);
+            list.removeChild(deleteButton);
+        });
+
+         
+        list.appendChild(taskButton);
+        list.appendChild(deleteButton);
+
+         
+         
+    }  
+});
